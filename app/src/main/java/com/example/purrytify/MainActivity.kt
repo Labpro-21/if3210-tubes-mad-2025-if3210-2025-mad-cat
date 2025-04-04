9e58166c13e37fc34eac2de082955d9456550d73
Yasmin Farisah Salma <114626408+caernations@users.noreply.github.com>
2025-04-04 15:20:57 +0700

    feat: main activity & navigation

--- app/src/main/java/com/example/purrytify/MainActivity.kt
+++ app/src/main/java/com/example/purrytify/MainActivity.kt
@@ -5,12 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.purrytify.ui.screens.HomeScreen
+import com.example.purrytify.ui.screens.LoginScreen
+import com.example.purrytify.ui.screens.SplashScreen
 import com.example.purrytify.ui.theme.PurrytifyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +21,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PurrytifyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "splash"
+                    ) {
+                        composable("splash") {
+                            SplashScreen(navController = navController)
+                        }
+
+                        composable("login") {
+                            LoginScreen(navController = navController)
+                        }
+
+                        composable("home") {
+                            HomeScreen(navController = navController)
+                        }
+
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PurrytifyTheme {
-        Greeting("Android")
     }
 }
