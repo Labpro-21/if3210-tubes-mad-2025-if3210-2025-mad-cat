5cb37897727819c09f2eb9be37ec2b24d500eff6
Yasmin Farisah Salma <114626408+caernations@users.noreply.github.com>
2025-04-04 15:19:36 +0700

    feat: splash screen

---
+++ app/src/main/java/com/example/purrytify/ui/screens/SplashScreen.kt
@@ -0,0 +1,42 @@
+package com.example.purrytify.ui.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.purrytify.R
+import kotlinx.coroutines.delay
+
+@SuppressLint("CoroutineCreationDuringComposition")
+@Composable
+fun SplashScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF121212)),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.purrytify_logo),
+            contentDescription = "Purrytify Logo",
+            modifier = Modifier.size(165.dp)
+        )
+    }
+
+    LaunchedEffect(key1 = true) {
+        delay(2000)
+        navController.navigate("login") {
+            popUpTo("splash") { inclusive = true }
+        }
+    }
+}
