377080a94dd84440a1ccc79bbf0037b4672549de
JonathanSaragih <jonathan.srgh@gmail.com>
2025-04-12 22:30:36 +0700

    add and fix searchbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.compose.material:material:1.2.0")
+    implementation(libs.androidx.appcompat.resources)
 
     // Testing
     testImplementation(libs.junit)
